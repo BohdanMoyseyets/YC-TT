aa442dc0b7165060081398d2d5b284ac1daad043
BohdanMoyseyets <y.stetsko@bistox.com>
2022-08-10 02:53:49 +0300

    two pages without popup and without media

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,21 @@
-let sum = (a, b) => a + b;
-console.log(sum(12,5));
+function notificClick() {
+    let element = document.getElementsByClassName("notification");
+    let rightBlock = document.getElementsByClassName("right-menu");
+    console.log(element);
+    element[0].classList.toggle("notification-active");
+    rightBlock[0].classList.toggle("open");
+}
+
+function openTab(evt, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("tab-content");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" tablink_active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " tablink_active";
+}
